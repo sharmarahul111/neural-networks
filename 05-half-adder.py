59b361aa0fab8f77c1f5cc0b79270461e89ff00f
Pramendra Sharma <sharmarahul111.psr@gmail.com>
2026-05-22 18:23:26 +0545

    half adder

---
+++ 05-half-adder.py
@@ -0,0 +1,40 @@
+import numpy as np
+from network import Network
+train_adder = np.array([
+	[0,0,0,0],
+	[0,1,0,1],
+	[1,0,0,1],
+	[1,1,1,0]
+])
+
+inp = train_adder[0: , :2]
+expected = train_adder[0: , 2:]
+EPS = 1e-3
+RATE = 1e-1
+network = Network(2,2,2)
+print(network)
+
+error = network.cost(inp, expected)
+for i in range(20000):
+	gradient = network.finite_diff(inp, expected, eps=EPS)
+	# printing the gradient
+	# disp_gradient = Network(*network.design)
+	# disp_gradient.layers = gradient
+	# print("----------------------------")
+	# print(f"Gradient {i}:")
+	# print(disp_gradient)
+	# print("Cost:", network.cost(inp, expected))
+	# end print
+	network.descent(gradient, RATE)
+
+print(f"Cost: {error} => {network.cost(inp, expected)}")
+
+# print(error)
+
+print("----------------------------")
+print(network)
+for i in range(2):
+	for j in range(2):
+		result = network.forward(np.array([i,j]))
+		print(f"{i} + {j} = {result}")
+		
